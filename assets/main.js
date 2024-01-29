5a10db9f3085022edc1b7ffc0e8a112939ce7e8f
Antônio Filho <antonio.filho@mov.net.br>
2024-01-29 10:00:08 -0300

    sistema_frota v0.1.2

--- assets/main.js
+++ assets/main.js
@@ -1,66 +1,92 @@
 console.log("Estou aqui!");
 
-const inputClient = document.getElementById('input-client');
-const inputServiceOrder = document.getElementById('input-service-order');
-const inputEquipament = document.getElementById('input-equipament');
-const inputTeam = document.getElementById('input-team');
-const inputDriver = document.getElementById('input-driver');
-const inputObservation = document.getElementById('input-observation');
-const inputState = document.getElementById('input-state');
+const inputDate = document.getElementById("input-date");
+const inputClient = document.getElementById("input-client");
+const inputServiceOrder = document.getElementById("input-service-order");
+const inputEquipament = document.getElementById("input-equipament");
+const inputTeam = document.getElementById("input-team");
+const inputDriver = document.getElementById("input-driver");
+const inputObservation = document.getElementById("input-observation");
+const inputState = document.getElementById("input-state");
 //const inputEquipament = document.getElementById('input-equipament');
 
-const btn = document.querySelector('.btn')
-.addEventListener('click', e => {
-  if(!inputClient.value && !inputServiceOrder.value && !inputEquipament.value 
-    && !inputTeam.value && !inputDriver.value, !inputObservation.value && !inputState.value) return;
-  arrayData(inputClient.value, inputServiceOrder.value, inputEquipament.value, inputTeam.value, inputDriver.value, inputObservation.value, inputState.value);
+const btn = document.querySelector(".btn").addEventListener("click", (e) => {
+  if (
+    (!inputDate.value &&
+      !inputClient.value &&
+      !inputServiceOrder.value &&
+      !inputEquipament.value &&
+      !inputTeam.value &&
+      !inputDriver.value,
+    !inputObservation.value && !inputState.value)
+  )
+    return;
+  arrayData(
+    inputDate.value,
+    inputClient.value,
+    inputServiceOrder.value,
+    inputEquipament.value,
+    inputTeam.value,
+    inputDriver.value,
+    inputObservation.value,
+    inputState.value
+  );
 });
 
-const ul = document.querySelector('ul');
+const ul = document.querySelector("ul");
 
-const createLi = () =>{
-  const li = document.createElement('li');
+const createLi = () => {
+  const li = document.createElement("li");
   return li;
-}
+};
 
-const arrayData = (value0, value1, value2, value3, value4, value5, value6) =>{
+const arrayData = (
+  value0,
+  value1,
+  value2,
+  value3,
+  value4,
+  value5,
+  value6,
+  value7
+) => {
   const arrays = [];
-  arrays.push(value0, value1, value2, value3, value4, value5, value6);
+  arrays.push(value0, value1, value2, value3, value4, value5, value6, value7);
   create(arrays);
-}
+};
 
-const create = (value1) =>{
+const create = (value1) => {
   const li = createLi();
   li.innerText = value1;
   ul.appendChild(li);
   save();
   clear();
-}
+};
 
-const save = () =>{
-  const listData = ul.querySelectorAll('li');
+const save = () => {
+  const listData = ul.querySelectorAll("li");
   const arraysData = [];
-  for(arraysDt of listData){
+  for (arraysDt of listData) {
     let dataArray = arraysDt.innerText;
     arraysData.push(dataArray);
   }
-  
+
   const arrayJSON = JSON.stringify(arraysData);
-  localStorage.setItem("frota", arrayJSON);
+  localStorage.setItem("FROTA", arrayJSON);
+};
 
-}
-
-function reload(){
-  const list = localStorage.getItem("frota");
+function reload() {
+  const list = localStorage.getItem("FROTA");
   listArray = JSON.parse(list);
 
-  for(let dataArray of listArray){
-      create(dataArray);
+  for (let dataArray of listArray) {
+    create(dataArray);
   }
 }
 
-const clear = () =>{
+const clear = () => {
   inputClient.focus();
+  inputDate.value = "";
   inputClient.value = "";
   inputServiceOrder.value = "";
   inputEquipament.value = "";
@@ -68,7 +94,6 @@ const clear = () =>{
   inputDriver.value = "";
   inputObservation.value = "";
   inputState.value = "";
-
-}
+};
 
 reload();
